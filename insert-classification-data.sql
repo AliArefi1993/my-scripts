648a1452aa3a6ece3e19e737435a1ee6ed31c7cd
Ali Arefi <ali.are@mtnirancell.ir>
2024-01-03 15:15:38 +0330

    Revert "sql scripts"
    This reverts commit b41116af676394fabc21ecd4fb12a071798aa6d4.

--- insert-classification-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.classifications(
-	name_translations)
-	VALUES ('"en"=>"Manufacture", "fa"=>"تولیدی"'), ('"en"=>"Technical Services", "fa"=>"خدمات فنی"')
-	, ('"en"=>"Commercial", "fa"=>"تجاری"');
